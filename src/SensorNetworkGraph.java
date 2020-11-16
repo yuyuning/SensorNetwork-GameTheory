baaab018e67edecc5e8963fa2cb8b90e614379fb
yuyuning <45050166+yuyuning@users.noreply.github.com>
2020-11-15 18:46:50 -0800

    Update SensorNetworkGraph.java

--- src/SensorNetworkGraph.java
+++ src/SensorNetworkGraph.java
@@ -1,12 +1,6 @@
-import java.awt.BasicStroke;
-import java.awt.Color;
-import java.awt.FontMetrics;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Point;
-import java.awt.RenderingHints;
-import java.awt.Stroke;
+import java.awt.*;
 import java.awt.geom.Ellipse2D;
+import java.awt.geom.Path2D;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -14,16 +8,15 @@ import java.util.Set;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-
 public class SensorNetworkGraph extends JPanel implements Runnable {
 	private static final long serialVersionUID = 1L;
 	
 	private Map<Integer, Axis> nodes;
 	private double graphWidth;
     private double graphHeight;
-    private int scaling = 25;
-    private int ovalSize = 6;
-    private int gridCount = 10;
+    private int scaling = 25; // 25
+    private int ovalSize = 10; // 6
+    private int gridCount = 10; // 10
     private boolean connected;
     private Map<Integer, Set<Integer>> adjList;
     private int[] dataGens;
@@ -80,7 +73,7 @@ public class SensorNetworkGraph extends JPanel implements Runnable {
        Graphics2D g2 = (Graphics2D) g;
        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-       double xScale =  ( (getWidth() - 3 * scaling) / (graphWidth));
+       double xScale =  ((getWidth() - 3 * scaling) / (graphWidth));
        double yScale =   (( getHeight() - 3 * scaling) / (graphHeight));
        
        List<Point> graphPoints = new ArrayList<Point>();
@@ -117,7 +110,7 @@ public class SensorNetworkGraph extends JPanel implements Runnable {
                int x1 = x0;
                int y0 = getHeight() - (2*scaling);
                int y1 = y0 - ovalSize;
-               if ((i % ((int) ((nodes.size() / 20.0)) + 1)) == 0) {
+               //if ((i % ((int) ((nodes.size() / 20.0)) + 1)) == 0) {
                if (nodes.size() > 0) {
                    g2.setColor(Color.black);
                    g2.drawLine(x0, getHeight() - (2*scaling) - 1 - ovalSize, x1, scaling);
@@ -128,56 +121,67 @@ public class SensorNetworkGraph extends JPanel implements Runnable {
                    
                }
                g2.drawLine(x0, y0, x1, y1);
-           }
+           //}
        }
 
        //Draw the edges
        Stroke stroke = g2.getStroke();
-       g2.setColor(Color.gray);
-       g2.setStroke(new BasicStroke(2f));
+       // pick color
+       Color darkBlue = new Color(0, 10, 100); // Color white
+       g2.setColor(darkBlue);
+       // control the width "?? f"
+       g2.setStroke(new BasicStroke(1f));
        for (int node: adjList.keySet()) {
     	   if((adjList.get(node) != null) && (!adjList.get(node).isEmpty())) {
-    	   for (int adj: adjList.get(node)) {
-	    	   if(adjList.get(node).contains(adj)) {
-	    			   int x1 = graphPoints.get(node-1).x;
-	    			   int y1 = graphPoints.get(node-1).y;
-	    			   int x2 = graphPoints.get(adj-1).x;
-	    			   int y2 = graphPoints.get(adj-1).y;
-	    			   g2.drawLine(x1, y1, x2, y2);
-
-	    	   }
-    	   }
+               for (int adj: adjList.get(node)) {
+                   if(adjList.get(node).contains(adj)) {
+                           int x1 = graphPoints.get(node-1).x;
+                           int y1 = graphPoints.get(node-1).y;
+                           int x2 = graphPoints.get(adj-1).x;
+                           int y2 = graphPoints.get(adj-1).y;
+                           g2.drawLine(x1, y1, x2, y2);
+                   }
+               }
     	   }
        }
 
        //Draw the oval
        g2.setStroke(stroke);
-       g2.setColor(Color.red);
+       // pick color
+       Color lightBlue = new Color(0, 110, 255); // Color white
+       g2.setColor(lightBlue);
        for (int i = 0; i < graphPoints.size(); i++) {
            double x = graphPoints.get(i).x - ovalSize / 2;
            double y = graphPoints.get(i).y - ovalSize / 2;
            double ovalW = ovalSize;
            double ovalH = ovalSize;
            Ellipse2D.Double shape = new Ellipse2D.Double(x, y, ovalW, ovalH);
+
+           boolean flag = false;
            for (int dg: dataGens){
                if(i+1==dg){
-                   x = graphPoints.get(i).x - (ovalSize+5) / 2;
-                   y = graphPoints.get(i).y - (ovalSize+5) / 2;
-                   shape = new Ellipse2D.Double(x, y, ovalW+5, ovalH+5);
-                   g2.fill(shape);
+                   x = graphPoints.get(i).x;
+                   y = graphPoints.get(i).y;
+                   g2.fill(createDefaultStar(5, x, y));
+                   g2.draw(createDefaultStar(5, x, y));
+                   flag = true;
                }
            }
-           g2.draw(shape);
 
+           if (!flag) {
+               g2.fill(shape);
+               g2.draw(shape);
+           }
        }
        
        //Label the nodes
-       g2.setColor(Color.blue);
-       for (int i = 0; i < graphPoints.size(); i++) {
-           int x = graphPoints.get(i).x - ovalSize / 2;
-           int y = graphPoints.get(i).y - ovalSize / 2;
-           g2.drawString(""+(i+1), x, y);
-       }
+//       g2.setColor(Color.red);
+//       for (int i = 0; i < graphPoints.size(); i++) {
+//           int x = graphPoints.get(i).x - ovalSize / 2;
+//           int y = graphPoints.get(i).y - ovalSize / 2;
+//           g2.setFont(new Font("TimesRoman", Font.PLAIN, 24));
+//           g2.drawString(""+(i+1), x, y);
+//       }
    }
 
 	public void run() {
@@ -189,4 +193,44 @@ public class SensorNetworkGraph extends JPanel implements Runnable {
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
    }
+
+    private static Shape createDefaultStar(double radius, double centerX, double centerY) {
+        return createStar(centerX, centerY, radius, radius * 2.63, 5,
+                Math.toRadians(-18));
+    }
+
+    private static Shape createStar(double centerX, double centerY,
+                                    double innerRadius, double outerRadius, int numRays,
+                                    double startAngleRad) {
+        Path2D path = new Path2D.Double();
+        double deltaAngleRad = Math.PI / numRays;
+        for (int i = 0; i < numRays * 2; i++)
+        {
+            double angleRad = startAngleRad + i * deltaAngleRad;
+            double ca = Math.cos(angleRad);
+            double sa = Math.sin(angleRad);
+            double relX = ca;
+            double relY = sa;
+            if ((i & 1) == 0)
+            {
+                relX *= outerRadius;
+                relY *= outerRadius;
+            }
+            else
+            {
+                relX *= innerRadius;
+                relY *= innerRadius;
+            }
+            if (i == 0)
+            {
+                path.moveTo(centerX + relX, centerY + relY);
+            }
+            else
+            {
+                path.lineTo(centerX + relX, centerY + relY);
+            }
+        }
+        path.closePath();
+        return path;
+    }
 }
